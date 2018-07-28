e39fe8a992682e39538ae86d88d0a04b26a1fded
constantineg1 <constantine.gurnov@gmail.com>
2018-07-28 15:06:59 -0700

    Base Architecture

---
+++ src/main/python/aihub/core/common/settings.py
@@ -0,0 +1,6 @@
+# Default settings file
+import os
+
+AIHUB_HOME = os.path.join(os.path.expanduser('~'), 'aihub')
+DATA_PATH = os.path.join(AIHUB_HOME, 'data')
+DATASETS_PATH = os.path.join(DATA_PATH, 'datasets')
